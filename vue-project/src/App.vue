82add3e31fd84407aca28821c01def88d7d0c400
Lam <huyenhuong3010@gmail.com>
2024-07-17 05:21:58 +0000

    add button

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,16 +5,11 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    hello lam  
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button>Click me</button>
+  
 </template>
 
 <style scoped>
