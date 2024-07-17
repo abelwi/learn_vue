f30280b43ef400d0787533d3fb708ee135de3d57
Lam <huyenhuong3010@gmail.com>
2024-07-17 06:10:34 +0000

    console log

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
 console.log('Hello world')
 
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const message = ref('Hello world')
-message.value = 'Changed'
+
+const counter = reactive({
+  count: 0
+})
+counter.count++
+
 </script>
 
 <template>
   <button>Click me</button>
   <h1>{{ message }}</h1>
-  
+  <p>Count is: {{ counter.count }}</p>
+
 </template>
 
 
