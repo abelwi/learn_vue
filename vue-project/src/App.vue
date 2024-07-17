6454253ff99cbc161d3d71b55d1e823241100b1a
Lam <huyenhuong3010@gmail.com>
2024-07-17 05:57:02 +0000

    console log

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 console.log('Hello world')
+
+import { ref } from 'vue'
+const message = ref('Hello world')
+message.value = 'Changed'
 </script>
 
 <template>
   <button>Click me</button>
-
+  <h1>{{ message }}</h1>
+  
 </template>
 
 
