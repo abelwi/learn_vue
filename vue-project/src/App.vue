d3da5dcbb1e9d616d79a3899e571073db9f394a0
Lam <huyenhuong3010@gmail.com>
2024-08-03 07:38:54 +0000

    learn vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,43 +1,15 @@
-<script setup>
-console.log('Hello world')
-
-import { ref } from 'vue'
- let id = 0
-
- const newTodo = ref('')
- const todos = ref([
-  { id: id++, text: 'Learn HTML'},
-  { id: id++, text: 'Learn CSS'},
-  { id: id++, text: 'Learn Javacript'}
- ])
-
- function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value })
-  newTodo.value = ''
- }
-
- function removeTodo(todo) {
-  todos.value = todos.value.filter(t => t !== todo );
- }
+<script>
+  export default {
+    data() {
+      return {
+        message: 'This is some text'
+      };
+    }
+  };
 </script>
-
 <template>
-  
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" required placeholder="new todo">
-    <button>Add todo</button>
-  </form>
-
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
-      <button @click="removeTodo(todo)">x</button>
-
-    </li>
-  </ul>
+  <h1>{{ message }}</h1>
 </template>
-
-
-
+<style></style>
 
 
