c6d7ae30132a581e0cbb105c7af02ce5f9d4a1e3
Lam <huyenhuong3010@gmail.com>
2024-08-05 14:02:50 +0000

    learn vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <h1 class="mt-16 text-2xl font-semibold text-center">Luyện tập writing task 2!</h1>
-  <div id="box">
-      <div id="type_text">
-      <textarea v-model="text" @input="adjustHeight" required placeholder="Type here"></textarea>
-      <button id="clear_button">x</button>
+  <div class="flex justify-center items-center">
+    <div class="text-center py-10 mt-16 w-1/2 border-2 border-gray-900 rounded-xl" id="box">
+      <h1 class="text-2xl font-semibold text-center">Luyện tập writing task 2!</h1>
+      <p class="mt-12 mb-3 text-left ml-20 font-semibold">Câu hỏi:</p>
+      <div class="mb-10" id="type_text">
+        <textarea 
+          class="w-2/3 text-lg border-b-2 border-gray-400 focus:border-black focus:outline-none font-serif"
+          v-model="text" 
+          @input="adjustHeight" 
+          required 
+          placeholder="Nhập câu hỏi của bạn..."
+        ></textarea>
       </div>
-      <button id="test">Click me</button>
-  </div>  
+      <button class="bg-lime-300 px-32 py-3 rounded-xl" id="test">Bắt đầu</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,9 +30,19 @@ export default {
       textarea.style.height = 'auto';
       textarea.style.height = textarea.scrollHeight + 'px';
     }
-  }
+  },
 };
 </script>
 
-
-
+<style scoped>
+  textarea {
+    outline: none;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    resize: none;
+  }
+  textarea:focus {
+    box-shadow: none;
+  }
+</style>
